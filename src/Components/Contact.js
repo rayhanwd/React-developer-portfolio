bb7b54fd9ec700622c2fd5c6584756880e8fd100
Kazi Rayhan <rayhanbd4400@gmail.com>
2021-06-08 14:38:37 +0600

    updated

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
                 <Row>
                     <Col md={4}>
                         <div className="mt-5">
-                            <div className="mb-4"><IoIosMail className="contact-icon" /> rayhanofficial2200@gmail.com</div>
+                            <div className="mb-4"><IoIosMail className="contact-icon" /> rayhanbd4400@gmail.com</div>
                             <div className="mb-4"><IoIosCall className="contact-icon" /> +8801992874495</div>
                             <div className="mb-4"><IoIosPin className="contact-icon" /> Jhenaidah,Bangladesh</div>
                         </div>
